8b19e2daaaf22e72f20b7d9f392a56ce62520ac1
Pedro Jesus <pedrojesus@Pedros-MacBook-Pro.local>
2025-05-18 19:35:49 -0300

    project fixes

---
+++ src/PJ.Gestures.Maui/AppBuilderExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PJ.Gestures.Maui;
+public static class AppBuilderExtensions
+{
+	public static MauiAppBuilder UsePjGestures(this MauiAppBuilder builder)
+	{
+
+		return builder;
+	}
+}
