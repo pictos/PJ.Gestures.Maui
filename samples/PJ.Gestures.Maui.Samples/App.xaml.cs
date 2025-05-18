08bfa2ebb706453bc8376a55a91f8836541d3f4a
Pedro Jesus <pedrojesus@Pedros-MacBook-Pro.local>
2025-05-18 19:18:20 -0300

    hello world

---
+++ samples/PJ.Gestures.Maui.Samples/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace PJ.Gestures.Maui.Samples;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+	}
+
+	protected override Window CreateWindow(IActivationState? activationState)
+	{
+		return new Window(new AppShell());
+	}
+}
