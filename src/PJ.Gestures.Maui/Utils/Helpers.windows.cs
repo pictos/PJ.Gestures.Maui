e9755d516dedcebfb7711353c5e94bbb797930f8
Pedro Jesus <pedrojesus.cefet@gmail.com>
2025-06-27 00:58:23 -0300

    Windows implementation (#6)
    * Enhance Windows support and gesture handling
    
    - Updated `Directory.Build.targets` to manage Windows-specific files.
    - Added null-conditional operator in `VerticalSwipeControl.cs` to prevent null reference errors.
    - Expanded `GestureBehavior.windows.cs` with comprehensive gesture recognition and event handling.
    - Introduced `Helpers.windows.cs` for converting Windows types to Maui-compatible types.
    
    * update xcode version

---
+++ src/PJ.Gestures.Maui/Utils/Helpers.windows.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.UI.Input;
+using WPoint = Windows.Foundation.Point;
+
+namespace PJ.Gestures.Maui.Utils;
+static class Helpers
+{
+	public static Point ToMauiPoint(this WPoint wPoint) =>
+		new(wPoint.X, wPoint.Y);
+
+	public static Vector2 ToMauiDistance(this ManipulationDelta delta) =>
+		new((float)delta.Translation.X, (float)delta.Translation.Y);
+}
